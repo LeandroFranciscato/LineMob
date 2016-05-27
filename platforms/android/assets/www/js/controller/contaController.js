6e90dab342e7e455092979a4ac3cc40ad9247966
Leandro <Leandro@LINEPACK-01>
2016-05-26 23:49:55 -0300

    Criação do FORMS de cadastro de contas.

--- platforms/android/assets/www/js/controller/contaController.js
+++ platforms/android/assets/www/js/controller/contaController.js
@@ -2,21 +2,22 @@
 
 var contaController = {
     TEMPLATE_CONTA_CADASTRO: "",
-    OBJECT_TO_BIND: "[data-content=content]",
-    load: function (cb) {
-        data = ""; // Implementar
-        Mustache.parse(this.TEMPLATE_CONTA_CADASTRO);
-        this.renderTemplateContaCadastro(data, function () {
-            if (cb) {
-                cb();
-            }
-        });
+    OBJECT_TO_BIND: "#scroller",
+    load: function (data, cb) {
+        if (!data) {
+            Mustache.parse(this.TEMPLATE_CONTA_CADASTRO);
+            this.render(data, function () {
+                if (cb) {
+                    cb();
+                }
+            });
+        }
     },
     render: function (data, cb) {
         data = (data) ? data : {};
         var html = Mustache.render(this.TEMPLATE_CONTA_CADASTRO, data);
         $(this.OBJECT_TO_BIND).html(html);
-        logUtil.log("Bind contaCadastro rendered.");
+        mainController.menuEsquerdo();
         if (cb) {
             cb();
         }
