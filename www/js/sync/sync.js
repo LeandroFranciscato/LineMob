985e0e720d88820db9dd977608cafe4e003dc0cc
Leandro <leandro_franciscato@hotmail.com>
2016-10-18 00:28:50 -0200

    Turn Better some translates and adding tips to help users!

--- www/js/sync/sync.js
+++ www/js/sync/sync.js
@@ -333,7 +333,7 @@ var sync = {
         });
     },
     ajax: function (httpType, responseType, url, dataInput, cbSuccess, cbError) {
-        url = "http://45.62.231.35:8080/LinemobAPI/" + url;
+        url = "http://10.0.0.102:8080/LinemobAPI/" + url;
         $.ajax({
             crossDomain: true,
             type: httpType,
